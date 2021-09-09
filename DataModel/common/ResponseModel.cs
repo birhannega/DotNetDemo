22d8bdfeb803931b6d42c6793a5fcec5c5ce2613
Tesafye Seifu <samsentes@gmail.com>
2021-09-09 10:45:11 +0300

    Return Data Type for ResponsModel Success should be  <T>

--- DataModel/common/ResponseModel.cs
+++ DataModel/common/ResponseModel.cs
@@ -5,7 +5,7 @@ namespace DataModel.common
     public class ResponseModel<T>
     {
         public List<T> Data { get; set; }
-        public ResponseModel<Employee> Success { get; set; }
+        public ResponseModel<T> Success { get; set; }
         public ErrorModel Error { get; set; }
         public int TotalCount { get; set; }
 
