db254e40b4e4e88e43c89187f06cc71d7b41642f
Teshale <teshaleyo2012@gmail.com>
2021-09-13 23:11:35 +0300

    pulled

--- Contracts/IDepartment.cs
+++ Contracts/IDepartment.cs
@@ -6,7 +6,7 @@ namespace Contracts
     public interface IDepartment
 
     {
-        public ResponseModel<Department> Create(Department department);  
+        public ResponseModel<Department> Create(Department department);
         public ResponseModel<Department> Update(int id,Department department);        
         public ResponseModel<Department> GetAll();
         public ResponseModel<Department> Get(int id);  
